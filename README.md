5d3145eedc8076da1db7ce96384227c4b2d413a7
Fox Gardner <fox@gardnermedia.com>
2020-08-20 22:19:09 -0600

    Added README.md file (+code comments)

---
+++ README.md
@@ -0,0 +1,14 @@
+Define. Words. Easy.
+
+### Requirements
+Hopefully you have Ruby.
+If needed, install Nokogiri gem with
+```
+gem install nokogiri -v 1.10.9
+```
+All other dependencies come with Ruby.
+
+### Instructions
+1. Place all words you wish to define on a new line in words.txt
+2. Run def_scraper.rb
+3. Open log.txt for all those wonderful definitions
--- def_scraper.rb
+++ def_scraper.rb
@@ -1,19 +1,17 @@
 require 'nokogiri' # Gem for Parsing
 require 'open-uri' # Wrapper for HTTP
 
-
-words_file = 'words.txt' # Opens words file
+words_file = 'words.txt' # Defines the wordfile. It is called in a handful of places.
 word_count = `wc -l "#{words_file}"`.split(' ')[0].to_i # Counts lines (words) in file
 
 puts "Words in Progress:"
 
-# Loops for each word
-word_count.times do |i|
+word_count.times do |i| # Loops for each word
     
     word = IO.readlines("#{words_file}")[i] # Picks word from file given the line number
-    puts word # Prints word into terminal.
+    puts word # Prints that word into terminal.
 
-    page = URI.open("https://www.dictionary.com/browse/#{word.strip}") # Inserts word into website URL
+    page = URI.open("https://www.dictionary.com/browse/#{word.strip}") # Inserts that word into website URL
     parse_page = Nokogiri::HTML(page) # Parses page
 
     definition = parse_page.css(".e1q3nk1v4").text # Collects the definition on website
